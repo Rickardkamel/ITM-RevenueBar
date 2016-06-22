cacac9ef4a08162842de21c8df13f481375ef504
Rickard Kamel <rickam@itm.local>
2016-06-22 10:56:12 +0200

    changed design

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ function executeCss(pxCalc) {
         })
 }
 
-
 $(document).ready(function () {
     var rawFile = new XMLHttpRequest();
     rawFile.open("GET", "resources/value.txt", true);
@@ -16,14 +15,27 @@ $(document).ready(function () {
             var value = 200 * (rawFile.responseText / 100000000);
 
             value = Math.round(value);
+            var revenue = Math.round(rawFile.responseText)
+            
+            var formattedRevenue = intToString(revenue);
 
-            var pxCalc = 200-value;
+            var pxCalc = 200 - value;
             executeCss(pxCalc);
+            document.getElementById("valueContainer").innerHTML = formattedRevenue;
+
+
         }
     }
     rawFile.send();
-
 });
+
+function intToString (value) {
+    var suffixes = ["", "k", "m", "b","t"];
+    var suffixNum = Math.floor((""+value).length/3);
+    var shortValue = parseFloat((suffixNum != 0 ? (value / Math.pow(1000,suffixNum)) : value).toPrecision(2));
+    if (shortValue % 1 != 0)  shortNum = shortValue.toFixed(1);
+    return shortValue+suffixes[suffixNum];
+}
 
 
 
